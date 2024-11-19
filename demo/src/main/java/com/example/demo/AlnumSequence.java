36e20fcb0555f3efeec99db7fa2957600cce985e
no1rain <no1rain@gmail.com>
2024-11-19 17:48:45 +0900

    Edit1. Generating an alphabetic sequence in Java

--- demo/src/main/java/com/example/demo/AlnumSequence.java
+++ demo/src/main/java/com/example/demo/AlnumSequence.java
@@ -5,79 +5,126 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class AlnumSequence {
-    
-    private static final Log LOG = LogFactory.getLog(App.class);
+    /** 알파벳 갯수 */
+    private static final int ALPHABET_COUNT = 26;
 
-    static final String MASKING_STR = "*";
+    /** 일련번호 초과 값 */
+    private static final int BASE_NUMBER = 100;
+
+    /** 일련번호 출력 값 */
+    private static final String ATTRIB_STR = "AZ";
 
     public static void main(String[] args) throws Exception {
         HashMap<String, Object> map = new HashMap<String, Object>();
+
         map.put("attr_lvl", 1);
-        map.put("attr_cls", "AA");
+        map.put("attr_cls", ATTRIB_STR);
         map.put("start_no", "95");
-        map.put("end_no", "QQ");
-        // map.put("end_no", "1A");
+        map.put("end_no", "BC");
 
-        String startNo = String.valueOf(map.get("start_no"));
-        String endNo = String.valueOf(map.get("end_no"));
-
-        int seq = getAlnumToNumber(map);
-        System.out.println("seq=====" + seq);
-
-        for(String s: endNo.split("")){
-            System.out.println(s + " ");
+        int sNum = getAlnumToNumber(map, "start_no");
+        int eNum = getAlnumToNumber(map, "end_no");
+        if(sNum < 0 || eNum < 0) {
+            System.out.println("구분이 잘못되었거나, 범위를 벗어났습니다.");
+            return;
         }
-        for(int i=0; i < endNo.length(); i++) {
-            LOG.info("i====" + endNo.charAt(i));
+        if(sNum > eNum) {
+            System.out.println("시작번호가 종료번호 보다 클 수 없습니다.");
+            return;
+        }
+
+        while(sNum <= eNum) {
+            System.out.println(sNum + "=========>" + createNumberToAlnum(sNum, ATTRIB_STR));
+            sNum++;
         }
     }
 
-    public static int getAlnumToNumber(Map<String, Object> param) {
-        String[] alpha = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        int idx = -1;
+    private static String createNumberToAlnum(int seq, String attrCls) {
+        String[] alphabets = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        String returnStr = "";
+
+        try {
+            StringBuilder sb = new StringBuilder();
+
+            switch(attrCls) {
+                case "AA":
+                case "AZ":
+                case "ZZ":
+                    if(BASE_NUMBER > seq) return String.valueOf(seq);
+
+                    // 첫번째 영문 추출
+                    int quotient = (seq - BASE_NUMBER) / ALPHABET_COUNT;
+                    // 두번째 영문 추출(나머지)
+                    int remainde = (seq - BASE_NUMBER) % ALPHABET_COUNT;
+
+                    // 첫번째 영문 + 두번째 영문 연결하기.
+                    sb.append(alphabets[quotient]);
+                    sb.append(alphabets[remainde]);
+
+                    returnStr = sb.toString();
+                    break;
+                case "0A":
+                case "0Z":
+                case "9A":
+                case "9Z":
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("createNumberToAlnum Exception##" + e.getMessage());
+        }
+        return returnStr;
+    }
+
+    public static int getAlnumToNumber(Map<String, Object> map, String column) {
+        String[] alphabets = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         int sequence = 0;
+        int idx = -1;
 
-        String attrCls = String.valueOf(param.get("attr_cls"));
-        String tNumber = String.valueOf(param.get("start_no"));
+        String rangeNo = String.valueOf(map.get(column));
 
+        String attrCls = String.valueOf(map.get("attr_cls"));
         System.out.println("attrCls=====" + attrCls);
         switch(attrCls) {
             case "A0":
             case "A9":
-                if(isNumeric(tNumber)) return Integer.parseInt(tNumber);
+                if(rangeNo.length() != 2) return idx;
+                if(isNumeric(rangeNo)) return Integer.parseInt(rangeNo);
 
-                idx = Arrays.asList(alpha).indexOf(tNumber.substring(0, 1));    // 영문 추출
+                idx = Arrays.asList(alphabets).indexOf(rangeNo.substring(0, 1));    // 영문 추출
                 if(idx > -1) {
-                    sequence = 100 + (10*idx) + Integer.parseInt(tNumber.substring(1, 2));
+                    sequence = 100 + (10*idx) + Integer.parseInt(rangeNo.substring(1, 2));
                     //(9*idx) : A1~9까지 사용시, (10*idx) : A0~A9까지 사용시
                 }
                 break;
             case "0A":
             case "0Z":
-                if(isNumeric(tNumber)) return Integer.parseInt(tNumber);
+            case "9A":
+            case "9Z":
+                if(rangeNo.length() != 2) return idx;
+                if(isNumeric(rangeNo)) return Integer.parseInt(rangeNo);
 
-                idx = Arrays.asList(alpha).indexOf(tNumber.substring(1, 2));    // 영문 추출
+                idx = Arrays.asList(alphabets).indexOf(rangeNo.substring(1, 2));    // 영문 추출
                 if(idx > -1) {
-                    sequence = 100 + idx + (26 * (Integer.parseInt(tNumber.substring(0, 1)) -1));
+                    sequence = 100 + idx + (26 * (Integer.parseInt(rangeNo.substring(0, 1)) -1));
                 }
                 break;
-            case "AZ":
             case "AA":
+            case "AZ":
             case "ZZ":
-                if(isNumeric(tNumber)) return Integer.parseInt(tNumber);
+                if(rangeNo.length() != 2) return idx;
+                if(isNumeric(rangeNo)) return Integer.parseInt(rangeNo);
 
                 // 1. 00~ZZ (00~99,AA~ZZ)
-                idx = Arrays.asList(alpha).indexOf(tNumber.substring(0, 1));    // 1번째 영문 추출
+                idx = Arrays.asList(alphabets).indexOf(rangeNo.substring(0, 1));    // 1번째 영문 추출
                 if(idx == -1)
                     return idx;
 
                 sequence = 100 + (26 * idx);
 
-                idx = Arrays.asList(alpha).indexOf(tNumber.substring(1, 2));    // 2번째 영문 추출
+                idx = Arrays.asList(alphabets).indexOf(rangeNo.substring(1, 2));    // 2번째 영문 추출
                 if(idx == -1)
                     return idx;
                 
@@ -86,14 +133,8 @@ public class AlnumSequence {
                 break;
             case "A..Z":
                 break;
-            case "00":
-            case "000":
-            case "0000":
-            case "00000":
-            case "000000":
-                if(isNumeric(tNumber)) return Integer.parseInt(tNumber);
-                break;
             default:
+                if(isNumeric(rangeNo)) return Integer.parseInt(rangeNo);
                 break;
         }
         return sequence;
@@ -101,7 +142,7 @@ public class AlnumSequence {
 
     public static boolean isNumeric(String str) {
         Pattern pattern = Pattern.compile("[^0-9]");
-        return ! pattern.matcher(str).find();
+        return !pattern.matcher(str).find();
     }
 
     public static boolean isNumeric2(String str) {
